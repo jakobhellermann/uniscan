f2083a9315512243d3d71aa565d49cf94bdd9ca3
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-08-29 16:06:47 +0200

    cli: print amount and duration

--- src/bin/uniscan.rs
+++ src/bin/uniscan.rs
@@ -1,6 +1,7 @@
 use anyhow::{Context, Result};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::path::Path;
+use std::time::Instant;
 use std::usize;
 use uniscan::{ScriptFilter, UniScan};
 
@@ -13,11 +14,15 @@ fn main() -> Result<()> {
     let script_filter = args.next().context("missing name of Script")?;
     let filter = args.next();
 
+    let start = Instant::now();
+
     let script_filter = ScriptFilter::new(&script_filter);
     let uniscan = UniScan::new(Path::new(&game_dir), filter.as_deref().unwrap_or("."))?;
 
     let scan = uniscan.scan_all(&script_filter, usize::MAX)?;
     print_all(&scan.items);
+
+    println!("{} items in {:?}", scan.count, start.elapsed());
 
     Ok(())
 }
