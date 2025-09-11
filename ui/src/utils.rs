3d49cb84fd8921682fd2e610e00672481b4311eb
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-09-11 18:53:38 +0200

    log timings in info! instead of debug!

--- ui/src/utils.rs
+++ ui/src/utils.rs
@@ -1,4 +1,4 @@
-use tracing::debug;
+use tracing::info;
 
 const MIN_LOG_DURATION: std::time::Duration = std::time::Duration::from_millis(1);
 
@@ -6,7 +6,7 @@ pub fn time<T>(name: &'static str, f: impl FnOnce() -> T) -> T {
     let start = std::time::Instant::now();
     let res = f();
     if start.elapsed() > MIN_LOG_DURATION {
-        debug!("{name}: {:?}", start.elapsed());
+        info!("{name}: {:?}", start.elapsed());
     }
     res
 }
